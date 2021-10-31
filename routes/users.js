eec5b9b83bfc35a4a0d147f2c4257294446f4fed
samip779 <samipluitel28@gmail.com>
2021-10-31 20:17:39 +0545

    Created database models and finished with the user model validation

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
+
+const User = require("../models/User");
 
 // @route       POST        api/users
 // @desc        register a user
 // @access      Public
-router.post("/", (req, res) => {
-  res.send("registers a user");
-});
+router.post(
+  "/",
+
+  body("name", "Please add name").not().isEmpty(),
+  body("email", "Please enter a valid email").isEmail(),
+  body("password", "Please add a password with 6 or more characters").isLength({
+    min: 6,
+  }),
+
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send(req.body);
+  }
+);
 
 module.exports = router;
