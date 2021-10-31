0f513c8ec181a70073d126fe12c7c79083ee49b5
samip779 <samipluitel28@gmail.com>
2021-10-31 18:26:58 +0545

    test

--- server.js
+++ server.js
@@ -7,5 +7,3 @@ app.get("/", (req, res) => res.send({ msg: "welcome to contact keeper" }));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started at port ${PORT}`));
-
-// sdfsdsd
