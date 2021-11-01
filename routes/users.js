790f24c14acf729a1a47a6c529617a872064902e
samip779 <samipluitel28@gmail.com>
2021-11-01 09:57:48 +0545

    working on the users route

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const { body, validationResult } = require("express-validator");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
-
 const User = require("../models/User");
 const config = require("config");
 
